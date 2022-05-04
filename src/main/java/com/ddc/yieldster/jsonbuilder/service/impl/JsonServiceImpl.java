cf109c7d434bcfe45e57ccd78225022118cba770
Arun Sunny <as@ddc.dev>
2022-05-04 17:18:21 +0530

    feat: Added pseudocode for portfolio allocator

--- src/main/java/com/ddc/yieldster/jsonbuilder/service/impl/JsonServiceImpl.java
+++ src/main/java/com/ddc/yieldster/jsonbuilder/service/impl/JsonServiceImpl.java
@@ -112,13 +112,50 @@ JsonServiceImpl implements JsonService {
         Map<String,Integer> assetAllocation = (Map<String, Integer>) vaultSettings.get("assetAllocation");
 
         if (vaultSettings.get("forcedRebalance").equals(true) || (getNavOfAssets(vaultId,assetAllocation)>=(Double)vaultSettings.get("minTransactionSize"))) {
+            //TODO: ADD LOGIC IMPLEMENTATION HERE
+
+            /**
+             Data needed:
+                1) mapping of investable vault assets
+                2)
+
+             ---------------------------------------LOGIC-------------------------------------------
+             Variables
+             1) Tmin    := Minimum Transaction Size
+             2) AP1-APn := List of protocols to invest in (Sorted in Ascending order of allocation). Each AP[i], will have
+             {
+                protocolID,
+                protocolAllocation
+             }
+             3) VNAV  := VaultNAV
+             4) L1-Ln := List of investable assets (Sorted in Descending order of totalPrice). Each L[i], will have
+             {
+                balance,
+                totalPrice
+             }
+
+             Pseudocode
+             1) Get list of investable assets, L
+             2) While each of AP[i]
+                2.1) currentAllocation := priceOfProtocolToken(AP[i].protocolID) * ERC20<protocolID>.balanceOf(vaultId)  / VNAV * 100
+                2.2) if(currentAllocation<=AP[i].protocolAllocation)
+                    2.2.1) delta := AP[i].protocolAllocation - currentAllocation
+                    2.2.2) amountToInvest := delta * VNAV
+                    2.2.3) For each of L[j]
+                        A) If L[i].totalPrice >=Tmin && amountToInvest >= Tmin
+                            A.1) if L[j].totalPrice >=amountToInvest
+                                A.1.1) Move L[j].totalPrice / amountToInvest amount of assets to AP[i]
+                                A.1.2) Break
+                            A.2) else
+                                A.2.1) Move L[j].balance amounts of assets to AP[i]
+                        B) else, Increment i
+                    2.2.4) Increment i
+                2.3) else, increment i
+
+             ---------------------------------------------------------------------------------------
+             */
 
 
-
-
-
-
-        }
 
 
         return null;
